5e7d917079ec16498001d985013558422220643d
Mansi <mansi.kothari@bacancy.com>
2022-09-14 18:40:57 +0530

    Added colors, changes theme, added fonts & prepared folder structure

---
+++ app/src/main/java/com/tuc/retailpos/RetailPOSApplication.kt
@@ -0,0 +1,4 @@
+package com.tuc.retailpos
+
+object RetailPOSApplication {
+}
--- app/src/main/java/com/tuc/retailpos/ui/activity/MainActivity.kt
+++ app/src/main/java/com/tuc/retailpos/ui/activity/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.tuc.retailpos
+package com.tuc.retailpos.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tuc.retailpos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
