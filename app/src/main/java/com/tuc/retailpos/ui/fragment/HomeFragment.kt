a115532c435bf0791c5e4abf694ee55171c4378b
Mansi <mansi.kothari@bacancy.com>
2022-09-20 15:01:17 +0530

    Worked on Redeem dialog

--- app/src/main/java/com/tuc/retailpos/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/tuc/retailpos/ui/fragment/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.tuc.retailpos.ui.fragment
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.tuc.retailpos.databinding.DialogRedeemDealBinding
 import com.tuc.retailpos.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,6 +35,18 @@ class HomeFragment : Fragment() {
             cvReturn.setOnClickListener {
                 findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToInvoicesFragment())
             }
+            cvDeals.setOnClickListener { openRedeemDealDialog() }
         }
     }
+
+    private fun openRedeemDealDialog() {
+        val dealsDialog = Dialog(requireContext())
+        val binding = DialogRedeemDealBinding.inflate(layoutInflater)
+        dealsDialog.setContentView(binding.root)
+        binding.apply {
+            btnCancel.setOnClickListener { dealsDialog.dismiss() }
+        }
+        dealsDialog.show()
+    }
+
 }
